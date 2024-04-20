828df415f2f282d93c8762e8cc80af5f5966cac9
Brian Staruk <brian@staruk.net>
2024-04-20 17:09:01 -0400

    formatting

--- src/components/molecules/GlobalFooter.tsx
+++ src/components/molecules/GlobalFooter.tsx
@@ -4,7 +4,10 @@ import Wrapper from '@/components/atoms/Wrapper';
 
 const GlobalFooter = (): JSX.Element => {
   return (
-    <Wrapper className="flex flex-col gap-3 mt-6 pt-6 border-t border-gray-300" tagName="footer">
+    <Wrapper
+      className="flex flex-col gap-3 mt-6 pt-6 border-t border-gray-300"
+      tagName="footer"
+    >
       <Text>
         <Link href="https://github.com/bstaruk/brian-blog-next" target="_blank">
           View Source on GitHub
