ceb36e21086e2925d6fb883b36ee6d3e5c29ef4b
Brian Staruk <brian@staruk.net>
2024-04-20 14:49:55 -0400

    global header buildout

--- src/components/molecules/GlobalHeader.tsx
+++ src/components/molecules/GlobalHeader.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 import Link from '@/components/atoms/Link';
 import Text from '@/components/atoms/Text';
 import Wrapper from '@/components/atoms/Wrapper';
@@ -6,22 +10,42 @@ export type GlobalHeaderProps = {
   title?: string;
 };
 
+type MenuLinkFields = {
+  href: string;
+  title: string;
+};
+
+const menuLinks: MenuLinkFields[] = [
+  {
+    href: '/',
+    title: 'Home',
+  },
+  {
+    href: '/search',
+    title: 'Search Posts',
+  },
+];
+
 const GlobalHeader = ({ title }: GlobalHeaderProps): JSX.Element => {
+  const pathname = usePathname();
+
   return (
     <Wrapper className="flex flex-col gap-3 mb-6" tagName="header">
       <h1 className="text--h1">{title}</h1>
 
       <ul className="flex items-center gap-4">
-        <Text tagName="li">
-          <Link href="/">Home</Link>
-        </Text>
-
-        <Text tagName="li">
-          <Link href="/search">Search Posts</Link>
-        </Text>
+        {menuLinks.map((link, linkIndex) => (
+          <Text key={linkIndex} tagName="li">
+            {pathname === link.href ? (
+              <strong>{link.title}</strong>
+            ) : (
+              <Link href={link.href}>{link.title}</Link>
+            )}
+          </Text>
+        ))}
       </ul>
     </Wrapper>
-  )
+  );
 };
 
 export default GlobalHeader;
