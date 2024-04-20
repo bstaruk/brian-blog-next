8297a15d0b38431b82afd09678a6319300807cf4
Brian Staruk <brian@staruk.net>
2024-04-20 09:24:26 -0400

    importing .vscode & stylelint configs from starbase v4

---
+++ stylelint.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable-next-line import/no-anonymous-default-export */
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended'],
+  ignoreFiles: ['!**/*.css', 'node_modules'],
+  rules: {
+    /* Tailwind CSS Rules */
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'layer', 'config'],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme'],
+      },
+    ],
+
+    /* Other Rules */
+    'import-notation': null, // Allow string and url imports
+  },
+};
