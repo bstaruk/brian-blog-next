be055884aa8a8e8d8145bbdf99fa11ffa1f00df5
Brian Staruk <brian@staruk.net>
2024-04-21 13:39:22 -0400

    onCategoryClick init

--- src/components/molecules/PostSearchResult.tsx
+++ src/components/molecules/PostSearchResult.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+'use client';
+
 import classNames from 'classnames';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { Post } from '@/interfaces/post';
-import { postCategories } from '@/lib/api';
+import { postCategories } from '@/lib/api/postCategories';
 import DateTime from '@/components/atoms/DateTime';
 import Link from '@/components/atoms/Link';
 import Text from '@/components/atoms/Text';
@@ -12,7 +14,22 @@ type PostSearchResultProps = {
 };
 
 const PostSearchResult = ({ className, post }: PostSearchResultProps): JSX.Element => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const href = `/posts/${post.slug}`;
+
+  const onCategoryClick = (category: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (category) {
+      params.set('category', category);
+    } else {
+      params.delete('category');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
 
   return (
     <article className={classNames('flex flex-col', className)}>
@@ -42,6 +59,7 @@ const PostSearchResult = ({ className, post }: PostSearchResultProps): JSX.Eleme
             <Text tagName="li" variant="sm" key={catIndex}>
               <button
                 type="button"
+                onClick={() => onCategoryClick(cat)}
                 className="link--default p-1 rounded border border-transparent focus:outline focus:outline-1 focus:outline-eggplant-700 focus:border-eggplant-700"
               >
                 {postCategories[cat].title}
