af2ceddf08a1ebf05a27c8353a9679f647ebb6e3
Brian Staruk <brian@staruk.net>
2024-05-27 13:10:30 -0400

    removing old colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,24 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        eggshell: {
-          400: '#fcfcfb',
-          500: '#f2f0ec',
-          600: '#e8e4dd',
-          700: '#ddd8ce',
-          800: '#d3ccbf',
-        },
-        eggplant: {
-          200: '#625a7b',
-          300: '#56506c',
-          400: '#4b455e',
-          500: '#3f3a4f',
-          600: '#332f40',
-          700: '#282432',
-          800: '#1c1a23',
-        },
-      },
       fontFamily: {
         sans: ['var(--font-poppins), sans'],
       },
